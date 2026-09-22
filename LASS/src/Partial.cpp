3ad41af83746dcbf0e4a1920f50188f00931d21f
leyiQ <qianleyi0427@hotmail.com>
2026-09-22 18:26:44 -0500

    Cont.

--- LASS/src/Partial.cpp
+++ LASS/src/Partial.cpp
@@ -294,15 +294,12 @@ MultiTrack* Partial::render(int numChannels,
 	amplitude = amplitude + amptransient*amplitude;
 
 
-  /* Leyi Qian, Sep 2026: Ring Modulation: */
-
-
+  //Leyi Qian, Sep 2026: Ring Modulation
   rm = static_cast<m_value_type>(ring_mod_amp_it.next() * sin(2.0 * M_PI * rm_phase));
   rm_phase = pmod( rm_phase + (ring_mod_rate_it.next() / samplingRate) );
 
 	// Apply ring modulation to the amplitude already calculated above.
   amplitude = static_cast<m_value_type>(amplitude * rm);
-    /* Leyi Qian, Sep 2026: Ring Modulation: */
 
 
 
